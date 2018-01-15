fbde6f72fc9c46f390883d9d06573b55cff083e9
Filip Rogalski <f.rogalski@merixstudio.com>
2018-01-15 09:49:59 +0100

    Test some plugins and loaders

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,37 +1,49 @@
 import webpack from 'webpack';
 import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import fs from 'fs';
 
 const inProduction = (process.env.NODE_ENV === 'production');
 
 let webpackConfig = {
-    entry: './scripts/main.js',
+
+    entry: {
+        main: './scripts/main.js'
+    },
+
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, './_build'),
+        // filename: 'scripts/[name].[hash].js'
+        filename: 'scripts/[name].js'
     },
     
-    // resolve: {
-    //     root: [
-    //         __dirname,
-    //         __dirname, '/templates'
-    //     ]
-    // },
-
     module: {
         rules: [
-            {
-                test: /\.html$/,
-                use: 'nunjucks-loader',
-            },
+            // {
+            //     test: /\.html$/,
+            //     use: 'nunjucks-loader',
+            // },
 
             {
                 test: /\.s[ac]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader', 
+                    use: ['css-loader', 'sass-loader']
+                })
             },
 
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'] //works right to left
+            },
+
+            {
+                test: /\.jpe?g|gif|png$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
             },
 
             {
@@ -42,6 +54,28 @@ let webpackConfig = {
         ]
     },
     plugins: [
+        new ExtractTextPlugin({
+            filename: 'styles/main.css'
+        }),
+
+        new webpack.LoaderOptionsPlugin({
+            minimize: inProduction
+        }),
+
+        new CleanWebpackPlugin(['_build'], {
+            root: __dirname,
+            verbose: true
+        }),
+
+        function() {
+            this.plugin('done', stats => {
+                fs.writeFileSync(
+                    path.join(__dirname, '_build/manifest.json'),
+                    // JSON.stringify(stats.toJson().assetsByChunkName)
+                    JSON.stringify(stats.toJson())
+                )
+            })
+        }
     ]
 }
 
